5c96257121631cd9c20cf56b7d27cbc13adca0a8
Tanner Patterson <tpatterson5492@gmail.com>
2020-01-16 17:23:45 -0800

    filter added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => { 
-   getAllPups()
-})
+    document.addEventListener("DOMContentLoaded", () => { 
+        getAllPups()
+        getFilterButton() 
+    })
+    
+    const baseURL = "http://localhost:3000/pups"
+
+    const clearDogBar = () => {
+        let dogBar = document.getElementById("dog-bar")
+        while (dogBar.children[0]) {
+            dogBar.children[0].remove()
+        }
+    }
 
 const getAllPups = () => {
-    fetch("http://localhost:3000/pups")
+    clearDogBar()
+    fetch(baseURL)
     .then(res => res.json())
-    .then(json => singlePup(json))
-}
-
-const singlePup = (json) => {
-    json.forEach(pup => {
-        spanPupName(pup)
-    })
+    .then(json => json.forEach(pup => spanPupName(pup)))
 }
 
 const spanPupName = (pup) => {
-    let thisPup = pup
     let spanEl = document.createElement("span")
+    let thisPup = pup
     spanEl.classList.add(`pup-span`)
     spanEl.innerText = pup.name
     spanEl.onclick = () => {
@@ -26,12 +31,13 @@ const spanPupName = (pup) => {
 }
 
 const appendPup = (spanEl) => {
-    document.getElementById("dog-bar").appendChild(spanEl)
+    let dogSpanBar = document.getElementById("dog-bar")
+    dogSpanBar.appendChild(spanEl)
 }
 
 const showPup = thisPup => {
     const dogInfo = document.querySelector("#dog-info")
-        dogInfo.innerHTML = ""
+        dogInfo.textContent = ""
     let tPup = thisPup
     
     let pupImg = document.createElement("img")
@@ -57,10 +63,7 @@ const showPup = thisPup => {
             e.target.innerText = "Good Dog"
         }
         updateIsGoodBoy(myPup)
-}
-
-const baseURL = "http://localhost:3000/pups"
-
+    }
 
 const updateIsGoodBoy = (myPup) => {
     let opoGoodDog = myPup.isGoodDog
@@ -77,5 +80,40 @@ const updateIsGoodBoy = (myPup) => {
             })
         })
         .then(res => res.json())
-        .then(json => console.log(json))
+        .then(json => showPup(json))
+}
+
+const getFilterButton = () => {
+    document.getElementById("good-dog-filter")
+    .onclick = (e) => {
+        changeGoodDogFilterText(e)
+    }
+}
+
+const getDogsAgain = () => {
+    fetch(baseURL)
+    .then(res => res.json())
+    .then(json => allGoodDogs(json))
+}
+
+const changeGoodDogFilterText = (e) => {
+    if (e.target.innerText.includes("OFF")) {
+        e.target.innerText = "Filter good dogs: ON"
+        getDogsAgain()
+    } else {
+        e.target.innerText = "Filter good dogs: OFF"
+        getAllPups()
+    }
+}
+
+const allGoodDogs = (pups) => {
+    let goodDogs = []
+    clearDogBar()
+    pups.forEach(pup => {
+        pup.isGoodDog ? goodDogs.push(pup) : null
+    })
+    goodDogs.forEach(goodPup => {
+        spanPupName(goodPup)
+    })
+    goodDogs = []
 }
